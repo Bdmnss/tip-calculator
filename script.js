30644fbb1cda54f9d6e8ad0749ccf3c3a0d41d44
Bdmnss <lukabadali123@gmail.com>
2024-05-16 20:12:20 +0400

    add reset button logic

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const peopleNumber = document.querySelector(".people-input");
 const totalPrice = document.querySelector("#total-price")
 const buttons = document.querySelector(".buttons")
 const tipInput = document.querySelector(".tip-input")
+const resetButton = document.querySelector(".reset-button")
 let billInputValue;
 let peopleInputValue;
 let selectedTip;
@@ -11,10 +12,12 @@ let tipInputValue;
 
 billInput.addEventListener("input", e => {
     billInputValue = Number(e.target.value);
+    buttonActive(billInputValue);
 });
 
 peopleNumber.addEventListener("input", e => {
     peopleInputValue = Number(e.target.value);
+    buttonActive(peopleInputValue);
 });
 
 tipInput.addEventListener("input", e => {
@@ -35,6 +38,21 @@ buttons.addEventListener("click", e => {
     totalFunc();
 });
 
+resetButton.addEventListener("click", () => {
+    if(!resetButton.classList.contains("active")) {
+        return;
+    } else {
+        billInput.value = "";
+        billInputValue = "";
+        peopleInputValue = "";
+        selectedTip = "";
+        peopleNumber.value = "";
+        tipAmount.textContent = "0.00";
+        totalPrice.textContent = "0.00";
+        resetButton.classList.remove("active");
+    }
+})
+
 function tipAmountFunc() {
     let tipAmountPerson = (billInputValue * (selectedTip / 100)) / peopleInputValue;
     tipAmount.textContent = tipAmountPerson;
@@ -44,4 +62,12 @@ function totalFunc() {
     let tipAmountPerson = (billInputValue * (selectedTip / 100)) / peopleInputValue;
     let total = (billInputValue / peopleInputValue) + tipAmountPerson;
     totalPrice.textContent = total;
+}
+
+function buttonActive(inputValue) {
+    if(inputValue == 0) {
+        resetButton.classList.remove("active")
+    } else {
+        resetButton.classList.add("active");
+    }
 }
